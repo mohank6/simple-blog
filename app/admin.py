7c1bbbef4bca90bdca7971ce9a80eba6e1855fab
Mohan <mohan@ramailo.tech>
2024-02-20 20:56:54 +0545

    regsiter models to admin

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from .models import Author, Category, Post
 
-# Register your models here.
+
+@admin.register(Author)
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('name', 'email', 'is_active', 'created_at', 'updated_at')
+    list_filter = ('is_active',)
+    search_fields = ('name', 'email')
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'created_at', 'updated_at')
+    search_fields = ('name',)
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'is_published', 'created_at', 'updated_at')
+    list_filter = ('is_published', 'categories')
+    search_fields = ('title', 'author__name')
