c1f2144c81d111fc7ad2d834bd2a882dd2d15a8e
mohank6 <159783242+mohank6@users.noreply.github.com>
2024-02-21 13:12:43 +0545

    update post model (#7)
    * add models
    
    * regsiter models to admin
    
    * add author accessor
    
    * register app urls
    
    * update base url
    
    * add user views
    
    * update category model
    
    * add choices to category
    
    * add category views
    
    * update post model

--- app/models.py
+++ app/models.py
@@ -62,12 +62,13 @@ class Category(BaseModel):
 
 
 class Post(BaseModel):
-    title = models.CharField(max_length=255)
+    title = models.CharField(max_length=255, unique=True)
     slug = models.SlugField()
     categories = models.ManyToManyField(Category, related_name='posts')
     author = models.ForeignKey(Author, on_delete=models.CASCADE, related_name='posts')
     content = models.TextField()
     is_published = models.BooleanField(default=False)
+    is_deleted = models.BooleanField(default=False)
 
     def __str__(self):
         return self.title
