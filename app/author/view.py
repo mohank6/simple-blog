84b3dfd04bf99ab567bc02eda98312ca624d111a
mohank6 <159783242+mohank6@users.noreply.github.com>
2024-02-23 09:30:25 +0545

    add reponse builder to author views (#17)

--- app/author/view.py
+++ app/author/view.py
@@ -1,86 +1,112 @@
 from .author import Author
 from .serializer import AuthorSerializer
-from rest_framework import status
 from rest_framework.decorators import api_view
-from rest_framework.response import Response
 from rest_framework.parsers import JSONParser
 from django.views.decorators.csrf import csrf_exempt
+from app.api import ResponseBuilder, api
 
 
 @api_view(['GET'])
 @csrf_exempt
 def get_all_authors(request):
     try:
+        response_builder = ResponseBuilder()
         authors = Author.get_all_authors()
         serializer = AuthorSerializer(authors, many=True)
-        return Response(serializer.data, status=status.HTTP_200_OK)
+        return response_builder.get_200_success_response(
+            "Authors fetched successfully.", serializer.data
+        )
     except ValueError as e:
-        return Response(
-            {'message': 'No authors found'}, status=status.HTTP_404_NOT_FOUND
+        return response_builder.get_404_not_found_response(
+            error_code=api.AUTHOR_NOT_FOUND
         )
     except Exception as e:
-        return Response({'error': str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+        return response_builder.get_500_internal_server_error_response(
+            error_code=api.INTERNAL_SERVER_ERROR, errors=str(e)
+        )
 
 
 @api_view(['GET'])
 @csrf_exempt
 def get_author_by_id(request, id):
     try:
+        response_builder = ResponseBuilder()
         author = Author.get_author_by_id(id)
         serializer = AuthorSerializer(author)
-        return Response(serializer.data, status=status.HTTP_200_OK)
+        return response_builder.get_200_success_response(
+            "Author fetched successfully.", serializer.data
+        )
     except ValueError as e:
-        return Response(
-            {'message': 'Author not found'}, status=status.HTTP_404_NOT_FOUND
+        return response_builder.get_404_not_found_response(
+            error_code=api.AUTHOR_NOT_FOUND
         )
     except Exception as e:
-        return Response({'error': str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+        return response_builder.get_500_internal_server_error_response(
+            error_code=api.INTERNAL_SERVER_ERROR, errors=str(e)
+        )
 
 
 @api_view(['POST'])
 @csrf_exempt
 def create_author(request):
     try:
+        response_builder = ResponseBuilder()
         data = JSONParser().parse(request)
         serializer = AuthorSerializer(data=data)
         if serializer.is_valid():
             serializer.save()
-            return Response(serializer.data, status=status.HTTP_200_OK)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+            return response_builder.get_201_success_response(
+                "Author created successfully.", serializer.data
+            )
+        return response_builder.get_400_bad_request_response(
+            error_code=api.INVALID_INPUT, errors=serializer.errors
+        )
     except Exception as e:
-        return Response({'error': str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+        return response_builder.get_500_internal_server_error_response(
+            error_code=api.INTERNAL_SERVER_ERROR, errors=str(e)
+        )
 
 
 @api_view(['PUT', 'PATCH'])
 @csrf_exempt
 def update_author(request, id):
     try:
+        response_builder = ResponseBuilder()
         data = JSONParser().parse(request)
         is_PATCH = request.method == 'PATCH'
         author = Author.get_author_by_id(id)
         serializer = AuthorSerializer(author, data=data, partial=is_PATCH)
         if serializer.is_valid():
             serializer.save()
-            return Response(serializer.data, status=status.HTTP_200_OK)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+            return response_builder.get_201_success_response(
+                "Author updated successfully.", serializer.data
+            )
+        return response_builder.get_400_bad_request_response(
+            error_code=api.INVALID_INPUT, errors=serializer.errors
+        )
     except ValueError as e:
-        return Response(
-            {'message': 'Author not found'}, status=status.HTTP_404_NOT_FOUND
+        return response_builder.get_404_not_found_response(
+            error_code=api.AUTHOR_NOT_FOUND
         )
     except Exception as e:
-        return Response({'error': str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+        return response_builder.get_500_internal_server_error_response_response(
+            error_code=api.INTERNAL_SERVER_ERROR, errors=str(e)
+        )
 
 
 @api_view(['DELETE'])
 @csrf_exempt
 def delete_author(request, id):
     try:
+        response_builder = ResponseBuilder()
         author = Author.get_author_by_id(id)
         Author.delete_author(id)
-        return Response(status=status.HTTP_204_NO_CONTENT)
+        return response_builder.get_204_no_content_response(message="Author deleted")
     except ValueError as e:
-        return Response(
-            {'message': 'Author not found'}, status=status.HTTP_404_NOT_FOUND
+        return response_builder.get_404_not_found_response(
+            error_code=api.AUTHOR_NOT_FOUND
         )
     except Exception as e:
-        return Response({'error': str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+        return response_builder.get_500_internal_server_error_response(
+            error_code=api.INTERNAL_SERVER_ERROR, errors=str(e)
+        )
