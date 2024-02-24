91a75ddb07fc1f9088db8cfb2ec533226a570226
Mohan <mohan@ramailo.tech>
2024-02-24 15:47:47 +0545

    add reponse builder to category views

--- app/category/view.py
+++ app/category/view.py
@@ -5,97 +5,126 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework.parsers import JSONParser
 from django.views.decorators.csrf import csrf_exempt
+from app.api import ResponseBuilder, api
 
 
 @api_view(['GET'])
 @csrf_exempt
 def get_all_categories(request):
     try:
+        response_builder = ResponseBuilder()
         categories = Category.get_all_categories()
         serializer = CategorySerializer(categories, many=True)
-        return Response(serializer.data, status=status.HTTP_200_OK)
+        return response_builder.get_200_success_response(
+            message="Categories fetched", result=serializer.data
+        )
     except ValueError as e:
-        return Response(
-            {'message': 'No categories found'}, status=status.HTTP_404_NOT_FOUND
+        return response_builder.get_404_not_found_response(
+            error_code=api.CATEGORY_NOT_FOUND
         )
     except Exception as e:
-        return Response({'error': str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+        return response_builder.get_500_internal_server_error_response(
+            error_code=api.INTERNAL_SERVER_ERROR, errors=str(e)
+        )
 
 
 @api_view(['GET'])
 @csrf_exempt
 def get_category_by_id(request, id):
     try:
+        response_builder = ResponseBuilder()
         category = Category.get_category_by_id(id)
         serializer = CategorySerializer(category)
-        return Response(serializer.data, status=status.HTTP_200_OK)
+        return response_builder.get_200_success_response(
+            message="Category fetched", result=serializer.data
+        )
     except ValueError as e:
-        return Response(
-            {'message': 'Category not found'}, status=status.HTTP_404_NOT_FOUND
+        return response_builder.get_404_not_found_response(
+            error_code=api.CATEGORY_NOT_FOUND
         )
     except Exception as e:
-        return Response({'error': str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+        return response_builder.get_500_internal_server_error_response(
+            error_code=api.INTERNAL_SERVER_ERROR, errors=str(e)
+        )
 
 
 @api_view(['GET'])
 @csrf_exempt
 def get_category_by_name(request, name):
     try:
+        response_builder = ResponseBuilder()
         category = Category.get_category_by_name(name)
         serializer = CategorySerializer(category)
-        return Response(serializer.data, status=status.HTTP_200_OK)
+        return response_builder.get_200_success_response(
+            message="Category fetched", result=serializer.data
+        )
     except ValueError as e:
-        return Response(
-            {'message': 'Category not found'}, status=status.HTTP_404_NOT_FOUND
+        return response_builder.get_404_not_found_response(
+            error_code=api.CATEGORY_NOT_FOUND
         )
     except Exception as e:
-        return Response({'error': str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+        return response_builder.get_500_internal_server_error_response(
+            error_code=api.INTERNAL_SERVER_ERROR, errors=str(e)
+        )
 
 
 @api_view(['POST'])
 @csrf_exempt
 def create_category(request):
     try:
+        response_builder = ResponseBuilder()
         data = JSONParser().parse(request)
         serializer = CategorySerializer(data=data)
         if serializer.is_valid():
             serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(
-            {'message': serializer.errors}, status=status.HTTP_400_BAD_REQUEST
+            return response_builder.get_201_success_response(
+                message="Category created", result=serializer.data
+            )
+        return response_builder.get_400_bad_request_response(
+            error_code=api.INVALID_INPUT, errors=serializer.errors
         )
     except Exception as e:
-        return Response({'error': str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+        return response_builder.get_500_internal_server_error_response(
+            error_code=api.INTERNAL_SERVER_ERROR, errors=str(e)
+        )
 
 
 @api_view(['PUT', 'PATCH'])
 @csrf_exempt
 def update_category(request, id):
     try:
+        response_builder = ResponseBuilder()
         is_PATCH = request.method == 'PATCH'
         category = Category.get_category_by_id(id)
         data = JSONParser().parse(request)
         serializer = CategorySerializer(category, data=data, partial=is_PATCH)
         if serializer.is_valid():
             serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(
-            {'message': serializer.errors}, status=status.HTTP_400_BAD_REQUEST
+            return response_builder.get_201_success_response(
+                message="Category updated", result=serializer.data
+            )
+        return response_builder.get_400_bad_request_response(
+            error_code=api.INVALID_INPUT, errors=serializer.errors
         )
     except Exception as e:
-        return Response({'error': str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+        return response_builder.get_500_internal_server_error_response(
+            error_code=api.INTERNAL_SERVER_ERROR, errors=str(e)
+        )
 
 
 @api_view(['DELETE'])
 @csrf_exempt
 def delete_category(request, id):
     try:
+        response_builder = ResponseBuilder()
         category = Category.get_category_by_id(id)
         Category.delete_category(id)
-        return Response(status=status.HTTP_204_NO_CONTENT)
+        return response_builder.get_204_no_content_response(message="Category deleted")
     except ValueError as e:
-        return Response(
-            {'message': 'Category does not exists'}, status=status.HTTP_400_BAD_REQUEST
+        return response_builder.get_404_not_found_response(
+            error_code=api.CATEGORY_NOT_FOUND
         )
     except Exception as e:
-        return Response({'error': str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+        return response_builder.get_500_internal_server_error_response(
+            error_code=api.INTERNAL_SERVER_ERROR, errors=str(e)
+        )
