ffb0dc458639d2048e0fe0abd24deaea37067d2f
Mohan <mohan@ramailo.tech>
2024-02-25 20:45:50 +0545

    add response builder to comment

--- app/comment/view.py
+++ app/comment/view.py
@@ -5,94 +5,133 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework.parsers import JSONParser
 from django.views.decorators.csrf import csrf_exempt
+from app.api import ResponseBuilder, api
 
 
 @api_view(['GET'])
 @csrf_exempt
 def get_all_comments(request):
     try:
+        response_builder = ResponseBuilder()
         comments = Comment.get_all_comments()
         serializer = CommentSerializer(comments, many=True)
-        return Response(serializer.data, status=status.HTTP_200_OK)
+        return response_builder.get_200_success_response(
+            "Comments fetched successfully.", serializer.data
+        )
     except ValueError as e:
-        return Response(
-            {'message': 'No comments found'}, status=status.HTTP_404_NOT_FOUND
+        return response_builder.get_404_not_found_response(
+            error_code=api.COMMENT_NOT_FOUND
         )
     except Exception as e:
-        return Response({'error': str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+        return response_builder.get_500_internal_server_error_response(
+            error_code=api.INTERNAL_SERVER_ERROR, errors=str(e)
+        )
 
 
 @api_view(['GET'])
 @csrf_exempt
 def get_comment_by_id(request, id):
     try:
+        response_builder = ResponseBuilder()
         comment = Comment.get_comment_by_id(id)
         serializer = CommentSerializer(comment)
-        return Response(serializer.data, status=status.HTTP_200_OK)
+        return response_builder.get_200_success_response(
+            "Comment fetched successfully.", serializer.data
+        )
     except ValueError as e:
-        return Response(
-            {'message': 'No comment found'}, status=status.HTTP_404_NOT_FOUND
+        return response_builder.get_404_not_found_response(
+            error_code=api.COMMENT_NOT_FOUND
         )
     except Exception as e:
-        return Response({'error': str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+        return response_builder.get_500_internal_server_error_response(
+            error_code=api.INTERNAL_SERVER_ERROR, errors=str(e)
+        )
 
 
 @api_view(['GET'])
 @csrf_exempt
 def get_all_comments_of_post(request, id):
     try:
+        response_builder = ResponseBuilder()
         comments = Comment.get_all_comments_of_post(id)
         serializer = CommentSerializer(comments, many=True)
-        return Response(serializer.data, status=status.HTTP_200_OK)
+        return response_builder.get_200_success_response(
+            "Comments fetched successfully.", serializer.data
+        )
     except ValueError as e:
-        return Response(
-            {'message': 'No post or comments found'}, status=status.HTTP_404_NOT_FOUND
+        return response_builder.get_404_not_found_response(
+            error_code=api.COMMENT_NOT_FOUND
         )
     except Exception as e:
-        return Response({'error': str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+        return response_builder.get_500_internal_server_error_response(
+            error_code=api.INTERNAL_SERVER_ERROR, errors=str(e)
+        )
 
 
 @api_view(['POST'])
 @csrf_exempt
 def create_comment(request):
     try:
+        response_builder = ResponseBuilder()
         data = JSONParser().parse(request)
         serializer = CommentSerializer(data=data)
         if serializer.is_valid():
             serializer.save()
-            return Response(serializer.data, status=status.HTTP_200_OK)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+            return response_builder.get_201_success_response(
+                "Comment created successfully.", serializer.data
+            )
+        return response_builder.get_400_bad_request_response(
+            error_code=api.INVALID_INPUT, errors=serializer.errors
+        )
     except ValueError as e:
-        return Response({'message': 'No post found'}, status=status.HTTP_404_NOT_FOUND)
+        return response_builder.get_404_not_found_response(
+            error_code=api.COMMENT_NOT_FOUND
+        )
     except Exception as e:
-        return Response({'error': str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+        return response_builder.get_500_internal_server_error_response(
+            error_code=api.INTERNAL_SERVER_ERROR, errors=str(e)
+        )
 
 
 @api_view(['PUT', 'PATCH'])
 @csrf_exempt
 def update_comment(request, id):
     try:
+        response_builder = ResponseBuilder()
         is_PATCH = request.method == 'PATCH'
         comment = Comment.get_comment_by_id(id)
         data = JSONParser().parse(request)
         serializer = CommentSerializer(comment, data=data, partial=is_PATCH)
         if serializer.is_valid():
             serializer.save()
-            return Response(serializer.data, status=status.HTTP_200_OK)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+            return response_builder.get_201_success_response(
+                "Comment updated successfully.", serializer.data
+            )
+        return response_builder.get_400_bad_request_response(
+            error_code=api.INVALID_INPUT, errors=serializer.errors
+        )
     except ValueError as e:
-        return Response({'message': 'No post found'}, status=status.HTTP_404_NOT_FOUND)
+        return response_builder.get_404_not_found_response(
+            error_code=api.COMMENT_NOT_FOUND
+        )
     except Exception as e:
-        return Response({'error': str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+        return response_builder.get_500_internal_server_error_response(
+            error_code=api.INTERNAL_SERVER_ERROR, errors=str(e)
+        )
 
 
 @api_view(['DELETE'])
 @csrf_exempt
 def delete_comment(request, id):
     try:
+        response_builder = ResponseBuilder()
         comment = Comment.delete_comment(id)
-        return Response(status=status.HTTP_204_NO_CONTENT)
+        return response_builder.get_204_no_content_response(message="Comment deleted.")
     except ValueError as e:
-        return Response({'message': 'No post found'}, status=status.HTTP_404_NOT_FOUND)
+        return response_builder.get_404_not_found_response(
+            error_code=api.COMMENT_NOT_FOUND
+        )
     except Exception as e:
-        return Response({'error': str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+        return response_builder.get_500_internal_server_error_response(
+            error_code=api.INTERNAL_SERVER_ERROR, errors=str(e)
+        )
