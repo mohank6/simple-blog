dca677859344e50ddc124a9aed04d617e8aeaca5
Mohan <mohan@ramailo.tech>
2024-02-26 20:55:02 +0545

    update authentication classes

--- blog/settings.py
+++ blog/settings.py
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTStatelessUserAuthentication',
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ),
 }
