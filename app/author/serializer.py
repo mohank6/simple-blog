c8028c0bf3ba518689c78cb928607cfaf069d9b5
Mohan <mohan@ramailo.tech>
2024-02-26 19:04:37 +0545

    move signup to auth.py

--- app/author/serializer.py
+++ app/author/serializer.py
@@ -1,8 +1,15 @@
 from rest_framework import serializers
 from app.models import Author
+from django.contrib.auth.password_validation import validate_password
 
 
 class AuthorSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)
+
     class Meta:
         model = Author
-        fields = ['id', 'name', 'email', 'bio', 'created_at']
+        fields = ['id', 'name', 'email', 'bio', 'created_at', 'password', 'role']
+
+    def validate_password(self, value):
+        validate_password(value)
+        return value
--- app/author/view.py
+++ app/author/view.py
@@ -46,27 +46,6 @@ def get_author_by_id(request, id):
         )
 
 
-@api_view(['POST'])
-@csrf_exempt
-def create_author(request):
-    try:
-        response_builder = ResponseBuilder()
-        data = JSONParser().parse(request)
-        serializer = AuthorSerializer(data=data)
-        if serializer.is_valid():
-            serializer.save()
-            return response_builder.get_201_success_response(
-                "Author created successfully.", serializer.data
-            )
-        return response_builder.get_400_bad_request_response(
-            error_code=api.INVALID_INPUT, errors=serializer.errors
-        )
-    except Exception as e:
-        return response_builder.get_500_internal_server_error_response(
-            error_code=api.INTERNAL_SERVER_ERROR, errors=str(e)
-        )
-
-
 @api_view(['PUT', 'PATCH'])
 @csrf_exempt
 def update_author(request, id):
